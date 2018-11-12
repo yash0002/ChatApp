dd68403b63f70f5cd170bcda8859910487f07bfb
Yash <yash.sharma0002@gmail.com>
2018-11-12 11:52:11 +0530

    mon_commit

---
+++ src/component/detail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+function Welcome()
+{
+    return  (
+        <Button variant="contained" color=" blue ">Hello World</Button>
+    );
+}
