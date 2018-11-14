1ea80d6c8f13b970d3d8e7faaccf4d41b843b88e
Yash <yash.sharma0002@gmail.com>
2018-11-14 13:11:17 +0530

    after server created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,28 @@ import Login from './screen/login'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
+  state = {
+    data : null
+  };
+  
+  componentDidMount() {
+    //calling fetch function here once components mounts
+    this.callBackendAPI()
+    .then(res => this.setState ({ data : res.express}))
+    .catch(err => console.log(err))
+  }
+
+  //fetching our GET route from express server {only those route have to fetch}
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if(response.status !== 200) {
+      throw Error(body.message)
+    }
+    
+    return body;
+  }
   render() {
     return (
       <div>
