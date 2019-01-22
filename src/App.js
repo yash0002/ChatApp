bfaa53a5cefac5cbcffda60d543914de54efd410
Yash <yash.sharma0002@gmail.com>
2019-01-22 19:51:49 +0530

    some update on chatapp

--- src/App.js
+++ src/App.js
@@ -60,3 +60,20 @@ class App extends Component {
 }
 
 export default App;
+//-------------------------------Default App.js------------
+{/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div> */}
